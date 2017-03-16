d68aebfdd79b23ed8c5d06d6201673a030c7d625
David Lantos <david.lantos@gmail.com>
2017-03-16 18:42:10 +0000

    Expose routing as grapviz dot format

--- app/discover.rb
+++ app/discover.rb
@@ -1,36 +1,76 @@
 require 'hutch'
-require 'uri'
 require 'json'
+require 'logger'
+require 'uri'
 
 class Discover
   def initialize(rabbitmq_uri: ENV.fetch('RABBITMQ_URI', 'amqp://guest:guest@127.0.0.1:5672'),
                  rabbitmq_api_uri: ENV.fetch('RABBITMQ_API_URI', 'http://127.0.0.1:15672/'))
+    Hutch::Logging.logger = Logger.new(STDERR)
     Hutch::Config.set(:uri, rabbitmq_uri)
     URI(rabbitmq_api_uri).tap do |uri|
       Hutch::Config.set(:mq_api_host, uri.host)
       Hutch::Config.set(:mq_api_port, uri.port || (uri.scheme == 'https' ? 443 : 15672))
       Hutch::Config.set(:mq_api_ssl, uri.scheme == 'https')
     end
+    client
   end
 
   def client
+    return @client if @client
     Hutch.connect
-    Hutch.broker.api_client
+    @client = Hutch.broker.api_client
   end
 
-  def graph
-    {}.tap do |routes|
-      bound_queues.each do |queue|
-        routes[queue[:queue]] = route_to(queue)
-      end
-
-      bindings.each do |binding|
-        queue = routes[binding[:queue]]
-        next unless queue
-        queue[:routes] ||= []
-        queue[:routes] << route_from(binding)
-      end
+  def routes
+    items = {}
+    bindings.each do |binding|
+      queue_name = binding[:queue]
+      items[queue_name] ||= []
+      items[queue_name] << route_from(binding)
     end
+
+    bound_queues.each do |queue|
+      $stderr.print '.'
+      queue_name = queue[:queue]
+      items[queue_name] ||= []
+      items[queue_name].each { |route| route.merge!(route_to(queue)) }
+    end
+
+    items.flat_map { |_name, details| details }.reject(&:nil?)
+  end
+
+  def dot
+    puts "digraph G {"
+
+    puts '  subgraph Apps {'
+    puts '    node [shape=hexagon fillcolor=yellow style=filled]'
+    routes.map { |route| [route[:from_app], route[:to_app]] }.flatten.uniq.each do |app|
+      app = '    "' + app.to_s + '"'
+      app += '[fillcolor=red]' if app.to_s.empty?
+      puts app
+    end
+    puts '  }'
+
+    puts '  subgraph cluster_Entities {'
+    puts '    graph [style=invisible]'
+    puts '    node [shape=box fillcolor=turquoise style=filled]'
+    routes.map { |route| route[:entity] }.uniq.each do |entity|
+      puts '    "' + entity.to_s + '"'
+    end
+    puts '  }'
+
+    routes.each do |route|
+      edge = '  '
+      edge += [route[:from_app], route[:entity], route[:to_app]].map { |text| '"' + text.to_s + '"' }.join('->')
+      edge += '['
+      edge += ' label="' + route[:key].to_s.split('.')[2..-1]&.join('.').to_s + '"'
+      edge += ' color="red"' if route[:to_app].to_s.empty?
+      edge += ']'
+      puts edge
+    end
+
+    puts "}"
   end
 
   def bindings
