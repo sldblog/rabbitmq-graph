32843a99fe26a61d895c2159906944a4ab09f627
David Lantos <david.lantos@gmail.com>
2018-05-01 17:10:41 +0100

    Add tests around bindings discovery
    Bindings are used to discover what routing keys exist in the topology,
    so they provide the _publisher_ data.

--- spec/app/discover_spec.rb
+++ spec/app/discover_spec.rb
@@ -4,9 +4,9 @@ require 'spec_helper'
 require 'app/discover'
 
 RSpec.describe Discover do
-  let(:api_client) { double(:api_client) }
-
   describe '#new' do
+    let(:api_client) { double(:api_client) }
+
     it 'configures hutch based on HTTPS URL' do
       described_class.new(api_url: 'https://hostgiven/', api_client: api_client)
       expect(Hutch::Config[:mq_api_host]).to eq('hostgiven')
@@ -27,6 +27,70 @@ RSpec.describe Discover do
       expect(Hutch::Config[:mq_username]).to eq('usergiven')
       expect(Hutch::Config[:mq_password]).to eq('passwordgiven')
       expect(Hutch::Config[:mq_api_port]).to eq(12_345)
+    end
+  end
+
+  describe '#topology' do
+    let(:fake_output) { StringIO.new }
+    let(:api_client) { double(:api_client, bindings: bindings, queues: queues) }
+    let(:bindings) { [] }
+    let(:queues) { [] }
+
+    def register_queue_details_call(api_client, path, consumer_details)
+      body = { 'consumer_details' => consumer_details }
+      response = double(:response, body: body.to_json)
+      allow(api_client).to receive(:query_api).with(path: path).and_return(response)
+    end
+
+    let(:setup_transaction_queue) do
+      queues << { 'name' => 'transaction_queue', 'vhost' => '/' }
+      register_queue_details_call(
+        api_client,
+        '/queues/%2F/transaction_queue',
+        [
+          { 'consumer_tag' => 'payments-6f6924a1-9cbb-4add-b62c-441e36a9ff30',
+            'queue' => { 'vhost' => '/', 'name' => 'transaction_queue' } },
+          { 'consumer_tag' => 'payments-6701018f-12c8-445a-a8a5-5e80704d9b49',
+            'queue' => { 'vhost' => '/', 'name' => 'transaction_queue' } }
+        ]
+      )
+    end
+
+    subject(:topology) { described_class.new(api_client: api_client, output: fake_output).topology }
+
+    it 'connects publisher applications to consumer applications via routing keys and consumer tags' do
+      bindings << { 'destination' => 'transaction_queue', 'routing_key' => 'ledger.payment.made',
+                    'destination_type' => 'queue' }
+      setup_transaction_queue
+
+      expect(topology).to contain_exactly(
+        from_app: 'ledger',
+        to_app: 'payments',
+        entity: 'payment',
+        routing_key: %w[ledger payment made],
+        queue_name: 'transaction_queue'
+      )
+    end
+
+    it 'skips bindings that are circular' do
+      bindings << { 'destination' => 'same', 'routing_key' => 'same', 'destination_type' => 'queue' }
+      setup_transaction_queue
+
+      expect(topology).not_to include(hash_including(from_app: 'same'))
+    end
+
+    it 'skips bindings that do not have routing keys defined' do
+      bindings << { 'destination' => 'unprocessable', 'routing_key' => '', 'destination_type' => 'queue' }
+      setup_transaction_queue
+
+      expect(topology).not_to include(hash_including(from_app: ''))
+    end
+
+    it 'skips bindings that do not bind to queues' do
+      bindings << { 'destination' => '???', 'routing_key' => 'unknown_routing', 'destination_type' => 'unknown' }
+      setup_transaction_queue
+
+      expect(topology).not_to include(hash_including(from_app: 'unknown_routing'))
     end
   end
 end
