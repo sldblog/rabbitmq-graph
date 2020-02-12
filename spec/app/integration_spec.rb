34968802ba92a62da305c8d32876e488d9045dd8
David Lantos <david.lantos@gmail.com>
2020-02-12 22:20:42 +0000

    Add integration spec that can be run with a tag
    Use `bundle exec rspec --tag integration` to run this suite.
    
    It is currently a stub, awating the integration test runner and first
    example.

---
+++ spec/app/integration_spec.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+RSpec.describe 'integration', integration: true do
+  it 'executes a meaningless test' do
+    pending 'stub'
+    raise
+  end
+end
